ea74519c61f5a0dd2db89f61b39427cc37a4c195
mindaugas <blaivas1995@gmail.com>
2018-03-07 18:26:01 +0200

    init station

---
+++ modules/Student.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mindaugas
+ * Date: 18.3.7
+ * Time: 18.08
+ */
+
+class Student extends User
+{
+
+}
---
+++ modules/University.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mindaugas
+ * Date: 18.3.7
+ * Time: 18.16
+ */
+
+echo "labas";
+$student = Student();
+
+
+class University
+{
+
+}
