33eefd096ac2a70c90d78d2e6d6ab7b2591becb1
vilkyskis <arm.sablinskas@gmail.com>
2018-05-06 15:32:19 +0300

    Prfile updated with phone and address fields, editing added

---
+++ src/Form/AddressType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddressType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('state')
+            ->add('region')
+            ->add('settlement')
+            ->add('street')
+            ->add('number')
+            ->add('apartment')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Address::class,
+        ]);
+    }
+}
